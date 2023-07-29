ad67be910a9f488fda3f3b4cface71bfbd0880ef
JoacoVic <jnvicente17@gmail.com>
2023-07-29 17:36:37 -0300

    ejercicio primer sabado terminado

--- primer_clase_sabado/sabado.py
+++ primer_clase_sabado/sabado.py
@@ -32,45 +32,6 @@ Codigo de: Joaquin Vicente, división E
 
 """
 
-# class App(customtkinter.CTk):
-    
-#     def __init__(self):
-#         super().__init__()
-
-#         self.title("UTN FRA")
-#         self.minsize(320, 250)
-
-#         self.label_title = customtkinter.CTkLabel(master=self, text="Tour", font=("Arial", 20, "bold"))
-#         self.label_title.grid(row=0, column=0, columnspan=2, padx=20, pady=10)
-#         self.btn_mostrar = customtkinter.CTkButton(master=self, text="Mostrar", command=self.btn_mostrar_on_click)
-#         self.btn_mostrar.grid(row=3, pady=20, columnspan=2, sticky="nsew")
-#         self.label_nombre = customtkinter.CTkLabel(master=self, text="Nombre")
-#         self.label_nombre.grid(row=0, column=0, padx=20, pady=10)
-#         self.txt_nombre = customtkinter.CTkEntry(master=self)
-#         self.txt_nombre.grid(row=0, column=1)
-#         self.label_edad = customtkinter.CTkLabel(master=self, text="Edad")
-#         self.label_edad.grid(row=1, column=0, padx=20, pady=10)
-#         self.txt_edad = customtkinter.CTkEntry(master=self)
-#         self.txt_edad.grid(row=1, column=1)
-#         self.label2 = customtkinter.CTkLabel(master=self, text="Tipo")
-#         self.label2.grid(row=2, column=0, padx=20, pady=10)
-#         self.combobox_genero = customtkinter.CTkComboBox(master=self, values=["Masculino", "Femenino", "Otro"])
-#         self.combobox_genero.grid(row=2, column=1, padx=20, pady=10)
-   
-#     def btn_mostrar_on_click(self):
-#         nombre = self.txt_nombre.get()
-#         edad =  self.txt_edad.get()
-#         genero = self.combobox_genero.get()
-#         mensaje = f"Su nombre es {nombre}, tiene {edad} años de edad y su genero es {genero}"
-
-#         alert("Titulo", mensaje)
-        
-
-# if __name__ == "__main__":
-#     app = App()
-#     app.geometry("300x300")
-#     app.mainloop()
-
 class App(customtkinter.CTk):
    
     def __init__(self):
@@ -88,8 +49,6 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=4, pady=20, columnspan=2, sticky="nsew")
    
     def btn_mostrar_on_click(self):
-
-        # Puntos 1 y 3
         nombre = prompt("UTN", "Ingrese nombre")
         while not nombre or not nombre.isalpha() or len(nombre) < 3:
             nombre = prompt("UTN", "Error: ingrese nuevamente su nombre")
@@ -118,64 +77,72 @@ class App(customtkinter.CTk):
             mensaje_altura = "alto"
         else:
             mensaje_altura = "muy alto"
+        
+        mensaje = f"Usted es {nombre}, tiene {edad} años de edad, su genero es {genero} y según su altura es: {mensaje_altura}"
+
+        alert("UTN", mensaje)
 
 
         cantidad_exc = prompt("Excursiones", "Ingrese cantidad de excursiones")
         while not cantidad_exc or not cantidad_exc.isdigit() or int(cantidad_exc) < 0 or int(cantidad_exc) > 11:
             cantidad_exc = prompt("Excursiones", "Error: ingrese nuevamente la cantidad de excursiones")
+        cantidad_exc = int(cantidad_exc)
 
-        contador_exc = 0
-        precio_caro = None
-        tipo_mas_caro = ""
-        precio_barato = None
-        tipo_mas_barato = ""
-        promedio_precios = 0
-        suma_precios = 0
-        contador_caminatas = 0
-        contador_vehiculos = 0
+        """
+        5- una vez ingresada la cantidad se debe pedir por cada excursión 
+        el importe y el tipo de excursión(caminata  o vehículo). 
+        informar cual es el precio más caro el más barato y el promedio
 
-        while contador_exc < cantidad_exc:
+        6- informar cual es el tipo de excursión(caminata  o vehículo). 
+        más seleccionada o si se seleccionó las mismas veces (caminata  o vehículo)
+        """
 
-            importe =  prompt("UTN", "Ingrese importe")
+        importe_caro = 0
+        importe_barato = 0
+        bandera_caro = True
+        bandera_barato = True 
+        total_importes = 0
+        datos_cada_excursion = 0
+        contador_caminata = 0
+        contador_vehiculo = 0
+        contador_ambos = 0
+        while datos_cada_excursion < cantidad_exc:
+            datos_cada_excursion += 1
+            importe = prompt("Importe", "Ingrese el importe")
             while not importe or not importe.isdigit():
-                importe = prompt("UTN", "Error: ingrese nuevamente su importe")
+                importe = prompt("Error", "Por favor, ingrese el importe")
             importe = int(importe)
+            tipo_excursion = prompt("tipo", "Ingres el tipo de excursion (caminata o vehiculo)")
+            while not tipo_excursion or (tipo_excursion != "caminata" and tipo_excursion != "vehiculo"):
+                tipo_excursion = prompt("Error", "Por favor, ingrese el tipo de excursion (caminata o vehiculo)")
+            if bandera_caro == True or importe_caro < importe:
+                bandera_caro = False
+                importe_caro = importe
 
-            tipo = prompt("Excursiones", "Ingrese tipo de excursion (caminata o vehículo)")
-            while not tipo or not tipo.isalpha() or (tipo!= "caminata" and tipo!= "vehículo"):
-                tipo = prompt("Excursiones", "Error: Ingrese tipo de excursion (caminata o vehículo)")
+            if bandera_barato == True or importe_barato > importe:
+                bandera_barato = False
+                importe_barato = importe
 
-            if not precio_barato or importe < precio_barato:
-                precio_barato = importe
-                tipo_mas_barato = tipo
-            if not precio_caro or importe > precio_caro:
-                precio_caro = importe
-                tipo_mas_caro = tipo
-                
-            suma_precios += importe
-
-            contador_exc += 1
-
-            if tipo == "caminata":
-                contador_caminatas += 1
+            total_importes += importe
+            if tipo_excursion == "caminata":
+                contador_caminata += 1
             else:
-                contador_vehiculos += 1
+                contador_vehiculo += 1
 
+            contador_ambos += 1
         
-        promedio_precios = suma_precios / cantidad_exc
+        promedio_importe = total_importes / cantidad_exc
+        if contador_caminata == contador_vehiculo:
+            mensaje = f"La cantidad de excursiones de caminatas y vehículos es la misma ({contador_ambos})"
+        elif contador_caminata > contador_vehiculo:
+            mensaje = f"El tipo de excursion mas seleccionado es caminata"
+        else:
+            mensaje = f"El tipo de excursion mas seleccionado es vehiculo"
 
-        informe = f"La excursion mas barata vale ${precio_barato} y es de {tipo_mas_barato}.\n\
-            La excursion mas cara vale ${precio_caro} y es de {tipo_mas_caro}.\n\
-            El promedio de precios de las excursiones es de ${promedio_precios}"
-        alert("Informe", informe)
+        alert("Estadisticas", f"El precio mas caro es ${importe_caro} y el mas barato es ${importe_barato}\nEl promedio de los importes es ${promedio_importe}")
+        alert("Estadisticas 2", mensaje)
 
 
-
-        excursiones_int = int(cantidad_exc)
-
-        mensaje = f"Usted es {nombre}, tiene {edad} de edad y su genero es {genero} y según su altura es: {mensaje_altura}"
-
-        alert("UTN", mensaje)
 
 
 
